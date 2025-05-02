759474067b8b83b9086466b7f73cf3b6c3a78075
Abeeshan Sridaran <105498915+beemozzy221@users.noreply.github.com>
2025-05-03 01:02:20 +0530

    Bug fixed
    Some inconsistencies fixed

--- composite_sound_files/wav_converter.py
+++ composite_sound_files/wav_converter.py
@@ -1,14 +1,18 @@
 import os
 from pydub import AudioSegment
 from os.path import join as pjoin
+from pydub.utils import which
 
 def convert_wav(parent_sound_file_path):
+    print(which("ffmpeg"))
+    AudioSegment.converter = which("ffmpeg")
+
     for recording_no in os.listdir(parent_sound_file_path):
         for recording_file in os.listdir(pjoin(parent_sound_file_path, recording_no)):
-            print(pjoin(parent_sound_file_path, recording_no, recording_file))
+
             if recording_file.endswith(".mp3"):
                 # Load your MP3 file
-                sound = AudioSegment.from_file(pjoin(parent_sound_file_path, recording_no, recording_file), format="mp3")
+                sound = AudioSegment.from_mp3(pjoin(parent_sound_file_path, recording_no, recording_file))
 
                 # Export it as WAV
                 export_name = os.path.splitext(recording_file)[0] + ".wav"
