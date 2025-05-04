9ed0b6441bad0948d6da9b7cf220b02b611d5263
Abeeshan Sridaran <105498915+beemozzy221@users.noreply.github.com>
2025-05-04 23:41:50 +0530

    Update Process_Sound_Files.py

--- pages/Process_Sound_Files.py
+++ pages/Process_Sound_Files.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import os
 import model
 import waveletdecomp
-from pathlib import Path
 from os.path import join as pjoin
 
 # --------------CHATGPT GENERATED CODE (with some minor mods)----------------
@@ -52,14 +51,10 @@ st.title("🐦 Bird Sound Identifier")
 available_birds = [os.path.splitext(f)[0] for f in os.listdir(pjoin(mpr, WEIGHTS_DIR)) if f.endswith(".weights.h5")]
 bird_choice = st.multiselect("Select bird(s) to identify", available_birds, default=available_birds[:1])
 
-uploaded_file = st.file_uploader(label="Upload a WAV file")
+uploaded_file = st.file_uploader("Upload a WAV file")
 
 if uploaded_file is not None and bird_choice:
-
-    filename = Path(uploaded_file.name)
-    suffix = filename.suffix.lower()
-
-    st.audio(uploaded_file, format='wav/WAV/mp3')
+    st.audio(uploaded_file)
 
     # Spectrogram Generation
     if st.checkbox("Generate spectrogram?"):
