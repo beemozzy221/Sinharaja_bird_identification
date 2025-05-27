989e7f7dc3361da0ac82fa5540d0e8f37d926861
Abeeshan Sridaran <105498915+beemozzy221@users.noreply.github.com>
2025-05-27 07:01:13 +0530

    bug fix

--- pages/Process_Sound_Files.py
+++ pages/Process_Sound_Files.py
@@ -56,8 +56,8 @@ if uploaded_file is not None and bird_choice:
     if st.checkbox("Generate spectrogram?"):
         # Load audio
         spec_data, spec_sr = waveletdecomp.read_audio_files(uploaded_file)
-        spec_data = waveletdecomp.ensure_samples_channels(spec_data
-                                                          )
+        spec_data = spec_data.T
+
         # Generate spectrogram
         st.write("Spectrogram:")
         fig, ax = plt.subplots()
